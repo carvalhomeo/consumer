300c96909f9872068ad4a01d197083d823894811
Marcel Carvalho <48029185+carvalhomeo@users.noreply.github.com>
2022-11-24 14:02:20 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { Kafka, logLevel } from "kafkajs";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors);
+
+const server = http.createServer(app);
+const socket = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const kafka = new Kafka({
+  brokers: [process.env.BROKERS],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  },
+  logLevel: logLevel.NOTHING,
+});
+
+const consumer = kafka.consumer({ groupId: "coordinates" });
+
+socket.on("connect", (socket) => {
+  console.log("user connected: ", socket.id);
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("SERVER RUNNING ON PORT ", process.env.PORT);
+});
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "coordinates", fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("received: ", message?.value?.toString());
+      socket.emit("coordinates", message?.value?.toString());
+    },
+  });
+};
+
+run().catch(console.error);
